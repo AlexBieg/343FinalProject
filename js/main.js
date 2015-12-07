2326c787ac8a52955c07042f994810dc9dbbda91
YOUR NAME <kenseioh@uw.edu>
2015-12-07 14:28:28 -0800

    charities

--- js/main.js
+++ js/main.js
@@ -4,11 +4,8 @@ Parse.initialize("Wbo1H7gcYHPiHoWdEiPmDEC2SBXyzIac4VCPSFCL", "yiDwktPQEWp8Ea7K3Y
 var Products = Parse.Object.extend('Products');	
 
 //angular stuff
-<<<<<<< HEAD
 var myApp = angular.module('myApp', ['ui.router', 'firebase']);
-=======
-var myApp = angular.module('myApp', ['ui.router',]);
->>>>>>> 12f8118e1f88532e28bda217cc9881ad17b724e8
+
 
 //ui config
 myApp.config(function($stateProvider, $urlRouterProvider) {
@@ -106,19 +103,13 @@ myApp.controller('sellController', function($scope, $http) {
 	}
 });
 
-<<<<<<< HEAD
+
 myApp.controller('charityController', function($scope, $firebaseArray){
   var ref = new Firebase("https://charitylist.firebaseio.com/");
   var projRef = ref.child("projects");
   $scope.projects = $firebaseArray(projRef);
 })
-=======
-myApp.controller('cartController', function($scope, $http) {
-	var cart = Parse.User.current().get('cart');
-	console.log(cart);
-});
 
->>>>>>> 12f8118e1f88532e28bda217cc9881ad17b724e8
 //logout current user
 var logOutUser = function() {
 	Parse.User.logOut().then(function() {
