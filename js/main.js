acaf86356347a6e1148a4f205bd4f1df1996cb97
Colin Bartels <colin.0011@hotmail.com>
2015-12-09 13:38:02 -0800

    asfd

--- js/main.js
+++ js/main.js
@@ -65,29 +65,6 @@ myApp.controller('homeController', function($scope, $http) {
 		}
 	});
 
-	$scope.productsByDate = [];
-	var query = new Parse.Query(Products);
-	query.ascending("createdAt");
-	query.find({
-		success: function (results) {
-			console.log(results);
-			for (var i = 0; i < results.length; i++) {
-				var object = results[i];
-				var product = {
-					name: object.get('name'),
-					price: object.get('price'),
-					region: object.get('region'),
-					charity: object.get('charity'),
-					image: object.get('image'),
-					user: object.get('user'),
-					id: object.id
-				}
-				$scope.productsByDate.push(product);
-				$scope.$apply();
-			}
-		}
-	})
-
 	$scope.showIfLogged = function() {
 		return isLogged();
 	}
@@ -96,6 +73,8 @@ myApp.controller('homeController', function($scope, $http) {
 		addToCart(id);
 		showSuccess(id);
 	}
+
+	$scope.sortType = 'name'
 });
 
 //cart controller
